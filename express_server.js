4f5c7015a7407aea735dc2b32b01c7c524f650e7
Housam-j <housam-j@hotmail.com>
2021-11-10 01:26:26 +0000

    add urls_show

--- express_server.js
+++ express_server.js
@@ -17,17 +17,16 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- app.get("/urls", (req, res) => {
-   res.render("url_index", templateVars);
+app.get("/urls/:shortURL", (req, res) => {
+const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render("urls_show", templateVars)
+
+});
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
  });
  
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
